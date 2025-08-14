b1ea631704385949e6132bebc822dda98ed08e89
dterazhao <dterazhao@tencent.com>
2025-08-14 12:57:33 +0800

    update async api to list data

--- apps/web-mlstats/src/api/prods/index.ts
+++ apps/web-mlstats/src/api/prods/index.ts
@@ -3,12 +3,9 @@ import type { Recordable } from '@vben/types';
 import { requestClient } from '#/api/request';
 
 /**
- * 获取产品列表数据
+ * async api to list data
  */
-async function listData(
-  params: Recordable<any>,
-  reqPath: string = '/gift/search',
-) {
+async function listData(params: Recordable<any>, reqPath: string) {
   return requestClient.post<Array<object>>(`${reqPath}`, {
     ...params,
   });
